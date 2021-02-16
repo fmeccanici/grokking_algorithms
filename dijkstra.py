0e9f24da942e9b8b9cdf9c4ccc3a6787ecc91683
Floris Meccanici <floris@homedesignshops.nl>
2021-02-16 13:38:37 +0100

    breadth search and dijkstra complete

---
+++ dijkstra.py
@@ -0,0 +1,64 @@
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    
+    return lowest_cost_node
+
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+infinity = float("inf")
+
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = []
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
+path = []
+
+current_node = 'fin'
+path.append(current_node)
+
+while current_node is not 'start':
+    current_node = parents[current_node]
+    path.append(current_node)
+
+path = list(reversed(path))
+cost = costs['fin']
+
+print("Shortest path is " + str(path) + ", with cost " + str(cost))
